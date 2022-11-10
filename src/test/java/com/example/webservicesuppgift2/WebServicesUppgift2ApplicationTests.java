fe56ba77d904f2914a74c2a09c5a531872ab4b7d
Haris Avdicevic <haris.avdi93@gmail.com>
2022-11-10 16:10:54 +0100

    Initial commit

---
+++ src/test/java/com/example/webservicesuppgift2/WebServicesUppgift2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicesuppgift2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesUppgift2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
